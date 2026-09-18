cfec6f3c104c8cb51f68d7cdf4edca1d8f12ab47
OutsourcedCallers Content Routine <content-routines@outsourcedcallers.com>
2026-09-18 16:29:10 +0000

    Remove superseded September 18 research batch

--- app/data.ts
+++ app/data.ts
@@ -35,7 +35,6 @@ import { sep10ResearchPosts } from './sep10-research';
 import { sep11BlogPosts, sep11BlogDetails } from './sep11-blog';
 import { sep18BlogPosts, sep18BlogDetails } from './sep18-blog';
 import { sep11ResearchPosts } from './sep11-research';
-import { sep18ResearchPosts } from './sep18-research';
 import { sep18Run2ResearchPosts } from './sep18-run2-research';
 
 export const site = {
@@ -1610,7 +1609,6 @@ function makeResearchPost([slug, title, excerpt]: readonly [string, string, stri
 }
 
 export const researchPosts: Array<{ slug: string; title: string; excerpt: string; body: string[]; published: string; updated?: string; image?: string; handoff?: { href: string; label: string; text: string } }> = [
-  ...sep18ResearchPosts,
   ...sep18Run2ResearchPosts,
   ...sep11ResearchPosts,
   ...sep09ResearchPosts,
